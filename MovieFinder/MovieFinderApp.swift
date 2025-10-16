035d9edb5dcfe5cc5006b1541c49ba37e6d60d7a
MichaelKawwa <kawwamichael@gmail.com>
2025-10-16 14:39:48 -0400

    Initial Commit

---
+++ MovieFinder/MovieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderApp.swift
+//  MovieFinder
+//
+//  Created by michael kawwa on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
